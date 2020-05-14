7f2323c276d987f58789dd61389157711f8f7788
LEvinson2504 <ldsouza2504@gmail.com>
2020-05-14 20:18:04 +0530

    reverted data.json

--- src/components/progressbar/progress.js
+++ src/components/progressbar/progress.js
@@ -7,12 +7,12 @@ export var ProgressBar = ({ width, percent }) => {
 	const [value, setValue] = React.useState(0);
 
 	React.useEffect(() => {
-		setValue(percent * width);
+		setValue(percent * width / 100);
 	}, [percent, width]);
 
 	return (
 		<div>
-			<div className="progress-div" style={{ width: width }}>
+			<div className="progress-div" style={{ width: `${width}%` }}>
 				<div style={{ width: `${value}px` }} className="progress" />
 			</div>
 		</div>
