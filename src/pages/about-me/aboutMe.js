aa62d32ee83fb8d5ff10cdd355b6859c5f2c1d4d
LEvinson2504 <ldsouza2504@gmail.com>
2020-06-05 14:10:22 +0530

    uniformed aboutme page, need content for projects page and make it awesome

--- src/pages/about-me/aboutMe.js
+++ src/pages/about-me/aboutMe.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import "../../index";
 import Data from "../../components/data/data.json";
-import { motion } from "framer-motion";
+import "/home/levinson/Documents/project/portfolio/src/index.css"
+// import { motion } from "framer-motion";
 
 const list = {
 	hidden: {
@@ -48,91 +49,27 @@ class AboutMe extends Component {
 			return "Not loaded yet";
 		} else {
 			return (
-				<motion.div
-					class="container box margin-top"
-					initial="hidden"
-					animate="visible"
-					variants={list}
-				>
-					<div class="columns margin-top is-desktop">
-						<div class="column is-one-third">
-							<div class="card">
-								<figure class="image is-fullwidth">
-									<img
-										class=""
-										src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.mountainviewphoto.com%2Fwp-content%2Fuploads%2F2015%2F12%2Fheadshots-slide.jpg&f=1&nofb=1"
-										alt=""
-									/>
-								</figure>
-								<div class="card-content">
-									<h2 class="has-text-weight-bold title">
-										{this.state.data.name}
-									</h2>
-									<h3 class="subtitle">{this.state.data.about.job_title}</h3>
-								</div>
-							</div>
+				<section class="hero is-white is-fullheight">
+					<div class="container">
+						<div class="title">
+							<h1 class="about-me-heading"> Skills </h1>
+							{this.state.data.about.skills.map(skill => (
+								<h2> {skill.title} </h2>
+							))}
 						</div>
-						<div class="column is-two-thirds-desktop">
-							<h2 class="title has-text-dark"> Expertise </h2>
-							<div class="columns">
-								{this.state.data.about.expertise.map((item) => (
-									<div class="column">
-										<img src={item.imageLink ?? ""} alt="" />
-										<h2 class="is-size-4 has-text-black">{item.title}</h2>
-										<p class="is-size-5">{item.description}</p>
-									</div>
-								))}
-							</div>
-						</div>
-					</div>
-					<div class="columns is-desktop">
-						<div class="column has-text-justified">
-							<h2 class="title has-text-dark"> Summary </h2>
-							{/* Could make this a component */}
-							<p>
-								Elit qui adipisicing sint culpa ut tempor commodo exercitation
-								non proident do do et. Esse do laborum voluptate id sit et ad
-								excepteur aute nulla veniam amet ullamco labore. Proident tempor
-								duis consequat nostrud proident. Pariatur laborum mollit culpa
-								incididunt labore sit quis ad sit.
-							</p>
-							<p>
-								Culpa ut consequat mollit nulla nostrud magna dolor irure
-								commodo in. Dolor consequat ipsum amet velit voluptate magna qui
-								aute sit. Id in nulla est quis commodo sit sit in enim
-								exercitation. Irure quis veniam aliqua irroident laboris qui
-								dolore elit reprehenderit quis id. Duis officia adipisicing
-								laboris dolore sint et incididunt.
-							</p>
-							<p>
-								Consequat eu nostrud eiusmod aliqua id in ea amet esse dolor
-								cupidatat. Minim culpa non quis esse in aliquip consequat
-								pariatur eiusmod voluptate deserunt nulla id. Sunt ea mollit
-								sint aliquip et magna. Esse proident dolore do incididunt
-								proident nisi irure proident nulla ullamco adipisicing ullamco.
-								Ad reprehenderit cupidatat commodo duis duis ad aute incididunt
-								sit velit ipsum ex. Adipisicing tempor cillum nisi eu id
-								esse.Magna cillum velit laboris ut est in occaecat veniam ex et
-								nostrud dolor enim. Aliqua esse tempor exercitation excepteur.
-								Est qui mollit reprehenderit consectetur laborum.
-							</p>
-						</div>
-						<div class="column is-two-thirds">
-							<h2 class="title has-text-dark"> Skills </h2>
-							{/* Could make this a component */}
-							{this.state.data.about.skills.map((item) => (
-								<div class="column">
-									<p class="has-text-black subtitle">{item.title}</p>
-									<progress
-										class="progress is-primary"
-										value={item.proficiency}
-										max="100"
-									></progress>
-								</div>
+
+						<div class="title">
+							<h1 class="about-me-heading"> Books I read </h1>
+							{this.state.data.books.map(book => (
+								<>
+									<h2> {book.title} </h2>
+									<h3> - {book.author} </h3>
+								</>
 							))}
 						</div>
 					</div>
-				</motion.div>
+					<div className="hero-body"></div>
+				</section >
 			);
 		}
 	}
